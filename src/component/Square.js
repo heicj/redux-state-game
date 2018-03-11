951eb2188a1863f6acde4df313507cf01f3b210a
mewarren <holla@mewarren.com>
2018-03-11 14:50:21 -0700

    fix className with player.color

--- src/component/Square.js
+++ src/component/Square.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './square.css';
 
 export default class Square extends Component{
   render(){
-    const { id, onClick, value } = this.props;
+    const { id, onClick, value, state } = this.props;
     return (
       <div id={id} onClick={() => onClick(id)}>
-        {/* <div 
-          className={state[state.activePlayer].color}>
-        </div> */}
+        <div 
+          className = {state[value] ? state[value].color : ''}>
+        </div>
         {value}
       </div>
         
