f510c85b5c921c10303e0f89bd76995d4c106a11
mewarren <holla@mewarren.com>
2018-03-11 13:15:39 -0700

    add checkWinner fn

--- src/component/actions.js
+++ src/component/actions.js
@@ -1,8 +1,35 @@
 import { CHOICE_ADD } from './reducers';
 
 export function playerChoice(id){
-  return {
+  return (dispatch, getState) => {
+
+    dispatch({
     type: CHOICE_ADD,
     payload: id
+  });
+
+  const { gameBoard } = getState().game;
+  const winner = checkWinner(gameBoard);
+  console.log(winner);
   };
+}
+
+function checkWinner(gameBoard) {
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for(let i = 0; i < winCombos.length; i++) {
+    const [a, b, c] = winCombos[i];
+    if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+      return gameBoard[c];
+    }
+  }
+  return null;
 }
