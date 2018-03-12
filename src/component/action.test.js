d06d6a157254930c42363280e3fc5433dd502912
mewarren <holla@mewarren.com>
2018-03-12 12:53:58 -0700

    add reducer test

--- src/component/action.test.js
+++ src/component/action.test.js
@@ -1,7 +1,7 @@
 import { CHOICE_ADD } from './reducers';
 import { playerChoice } from './actions';
 
-it('creates a choice action', () => {
+it.skip('creates a choice action', () => {
   const { type, payload } = playerChoice(4);
   expect(type).toBe(CHOICE_ADD);
 });
