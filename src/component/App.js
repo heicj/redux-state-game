23f420fa2c054c0527441ed4add8a25990da72f4
Jack Lomax <lomax15@me.com>
2018-03-12 11:53:37 -0700

    functional game with minor css

--- src/component/App.js
+++ src/component/App.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import GameBoard from './Gameboard';
 import { connect } from 'react-redux';
 import { newGame } from './actions';
+import './App.css';
 
 class App extends Component{
   render(){
-    const { newGame } = this.props;
+    const { newGame, activePlayer } = this.props;
     return (
       <div>
-        <header> 
+        <header id='header'> 
           <h1>Connect 3</h1>
           <button onClick={newGame}> New Game </button>
         </header>
         <GameBoard/>
+        <section id="current">
+          <h1>
+            Current player: {activePlayer}
+          </h1>
+        </section>
       </div>
     );
   }
 }
 
 export default connect(
-  null, 
+  state => ({
+    activePlayer: state.game.activePlayer }), 
   { newGame }
 )(App);
