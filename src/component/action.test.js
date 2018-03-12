89febe53b469f73c55151271a34fb40a62a23a27
Jack Lomax <lomax15@me.com>
2018-03-12 16:37:55 -0700

    created functional choice action test

--- src/component/action.test.js
+++ src/component/action.test.js
@@ -1,7 +1,21 @@
 import { CHOICE_ADD } from './reducers';
 import { playerChoice } from './actions';
 
-it.skip('creates a choice action', () => {
-  const { type, payload } = playerChoice(4);
-  expect(type).toBe(CHOICE_ADD);
+it('creates a choice action', () => {
+  const index = 0;
+  const result = playerChoice(index);
+  const dispatch = jest.fn();
+  const getState = jest.fn(() => ({
+    game: {
+      gameBoard: ['', '', '', '', '', '', '', '', '']
+    }
+  }));
+  result(dispatch, getState);
+
+  expect(dispatch).toBeCalledWith({
+    type: 'CHOICE_ADD',
+    payload: index
+  });
+  expect(getState).toBeCalled();
 });
+  
